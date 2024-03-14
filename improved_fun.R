c1e7b653c6fd8d1067259fbafe59e77739840774
Jian <jian1.liu@wur.nl>
2024-03-15 00:15:50 +0100

    save updates

--- improved_fun.R
+++ improved_fun.R
@@ -4,12 +4,65 @@
 library(readxl)
 library(data.table)
 library(ggplot2)
+fontsize = 24
 # Import the normal and artificial weather data 
 # normal weather data
 weather_data <- read_excel('weather data Ludhiana India PULU0001.xlsx', skip = 4) |> as.data.table()
 weather_highTmax <- read_excel('weather data Ludhiana India PULU0001-increase T.xlsx', skip = 4) |> as.data.table()
 weather_data <- read_excel('weather.xlsx', .name_repair = "universal") |> as.data.table()
 
+thot <- 30.9 #threshold temperature value
+thotmin <- 25 #threshold temperature value
+thotmax <- 34 #threshold temperature value
+orig_weather <- weather_data
+orig_weather$tavg <- (orig_weather$`MAX TEMP °C (1seedling+0kgN)` + orig_weather$`MIN TEMP °C (1seedling+0kgN)`) / 2
+orig_weather$tavg <- orig_weather$tavg + 5
+orig_weather$strheat <- ifelse(orig_weather$tavg > thot,
+                               1.0 - 0.1*(orig_weather$tavg - thot),
+                               1)
+orig_weather$strheat <- ifelse(orig_weather$strheat  < 0,
+                               0, orig_weather$strheat )
+
+orig_weather$strheatmin <- ifelse(orig_weather$tavg > thotmin,
+                                  1.0 - 0.1*(orig_weather$tavg - thotmin),
+                                  1)
+orig_weather$strheatmin <- ifelse(orig_weather$strheatmin  < 0,
+                                  0, orig_weather$strheatmin )
+
+
+orig_weather$strheatmax <- ifelse(orig_weather$tavg > thotmax,
+                                  1.0 - 0.1*(orig_weather$tavg - thotmax),
+                                  1)
+orig_weather$strheatmax <- ifelse(orig_weather$strheatmax  < 0,
+                                  0, orig_weather$strheatmax )
+
+orig_weather_heading <- subset(orig_weather,DAS<81&DAS>73)
+orig_weather_heading$DAheading <- seq(0,6,1)
+
+Tavg_range <- seq(25,50,0.5)
+
+strheat <- ifelse(Tavg_range> thot,
+                  1.0 - 0.1*(Tavg_range - thot),
+                  1)
+
+
+Figure1 <- ggplot() + 
+  geom_line(aes(x = Tavg_range,y=strheat),size=2)+
+  xlab(expression(paste("Temperature (", degree, "C)")))+
+  ylab("Heat penalty on grain setting rate (-)")+theme_bw()+
+  theme(axis.line.x = element_blank(),axis.line.y = element_blank(),
+        panel.background =  element_blank(),panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.text = element_text(size=fontsize),
+        axis.title=element_text(size=fontsize),
+        axis.title.y.right=element_text(size=fontsize),
+        axis.line = element_line(colour = "black"))+
+  geom_vline(xintercept=thot,col="red")+
+  scale_y_continuous(limits=c(0, 1.1), breaks = seq(0,1,0.2),expand = c(0, 0))+
+  scale_x_continuous(limits=c(25, 45), breaks = seq(25,45,5))
+Figure1
+ggsave("orig_temp_curve.svg", height = 7, width = 8)
+# the improved fun --------------------
 setnames(weather_data, c("DAS", "GDD", "Tmax", "Tmin", "WeatherDate"))
 heading_st <- 74
 heading_ed <- heading_st + 7
@@ -108,13 +161,19 @@ hourly_combined |>
   scale_color_manual(name = "Real Temperature", values = c(Tmax = "red", Tmin = "blue"),
                      guide = guide_legend(override.aes = list(size = 6)))+
   labs(x = "Day after planting", 
-       y = "Temperature degree C")+
+       y = expression(paste("Temperature (", degree, "C)")))+
   theme_bw()+
-  theme(text = element_text(size = 16))+
+  theme(axis.line.x = element_blank(),axis.line.y = element_blank(),
+        panel.background =  element_blank(),panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.text = element_text(size=fontsize),
+        axis.title=element_text(size=fontsize),
+        axis.title.y.right=element_text(size=fontsize),
+        axis.line = element_line(colour = "black"))+
   scale_x_continuous(breaks = seq(min(hourly_combined$ID), max(hourly_combined$ID), by = 24), 
                      labels = seq(min(hourly_combined$DAS), max(hourly_combined$DAS), by = 1))
 
-ggsave("checktemp.png", dpi = 300 )
+ggsave("checktemp.svg", width = 9, height = 6)
 
 # testing TRF_GS
 TRF_GS_test <- TRF_GS(hourly_combined[day == DOY,]$Temp , 1000, GDD_START_HEADING, GDD_1_3)
@@ -134,7 +193,7 @@ new_grain_setting_rate <- data.table(DAS = heading_st: heading_ed, grain_setting
 prod(new_grain_setting_rate$grain_setting_rate)
 
 
-temp_fake <- seq(25, 40, 1)
+temp_fake <- seq(25, 45, 1)
 temp_fake_tmax <- temp_fake + 5
 temp_fake_tmin <- temp_fake - 5
 
@@ -173,10 +232,26 @@ for(GDD in GDD_range){
   i = i+ 1
 }
 
+
 b_over_dvs <- rbindlist(b_list)
-b_over_dvs |> 
+b_over_dvs[, GDD := GDD_range]
+b_over_dvs[, x_tick := paste0(round(DVS, 2), "\n(", GDD, ")")]
+fig2 <- b_over_dvs |> 
   ggplot(aes(DVS, b))+
-  geom_line()
+  geom_line(linewidth = 2) +
+  scale_x_continuous(breaks = round(b_over_dvs$DVS,2),labels = b_over_dvs$x_tick)+
+  labs(x = "DVS \n(GDD)",
+       y = "The magic parameter b") +
+  theme_bw()+
+  theme(axis.line.x = element_blank(),axis.line.y = element_blank(),
+        panel.background =  element_blank(),panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.text = element_text(size=fontsize),
+        axis.title=element_text(size=fontsize),
+        axis.title.y.right=element_text(size=fontsize),
+        axis.line = element_line(colour = "black"))
+ggsave("parameter b.png", dpi = 300)
+
 b_over_dvs[, id := 1:.N]
 hourly_combined
 hourly_temp_fake
@@ -196,73 +271,27 @@ b_dvs <- rbindlist(lapply(1:nrow(b_over_dvs), function(j) {
                          b_DVS = j)
 }), idcol = "id")
 
-b_dvs[b_over_dvs, on = "id"] |> 
-    ggplot(aes(temp_range, fertility, color = factor(DVS))) +
-    geom_line() +
-    labs(color = "DVS")
+b_dvs[b_over_dvs, on = "id"][, x_tick2 := paste0(round(DVS, 2), " (", GDD, ")")] |> 
+    ggplot(aes(temp_range, fertility, color = x_tick2)) +
+    geom_line(linewidth = 2) +
+  geom_vline(xintercept=tau_cultivar,col="red", linewidth = 1.5)+
+    theme_bw()+
+  xlim(c(25,45)) +
+  labs(x = expression(paste("Temperature (", degree, "C)")), 
+       y = "Heat penalty on grain setting rate (-)", 
+       color = "DVS (GDD)")+
+    theme(axis.line.x = element_blank(),axis.line.y = element_blank(),
+          legend.position = c(0.15, 0.3),
+          legend.text = element_text(size = fontsize -5),
+          legend.title = element_text(size = fontsize -5),
+        panel.background =  element_blank(),panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.text = element_text(size=fontsize),
+        axis.title=element_text(size=fontsize),
+        axis.title.y.right=element_text(size=fontsize),
+        axis.line = element_line(colour = "black"))+
+  scale_y_continuous(limits=c(0, 1.1), breaks = seq(0,1,0.2),expand = c(0, 0))
+  # scale_x_continuous(limits=c(, 40), breaks = seq(20,40,5))
 
-
-
-b_over_dvs[, hdi := HDI(Temp = )]
-
-# indian expeirment simulation suggests that we have the reproductive stage at DOY = 231
-
-HDI_test <- HDI(reprduct_temp[day == DOY,]$Temp)
-
-# Function to calculate DVS using GDD and a parameter P3
-# GDD is the growing degree days from the start of period 3
-tbase <- 10
-
-
-# testing 
-DVS_test <- calculate_DVS(1000, GDD_1_3, GDD_START_HEADING)
-
-b_test <- calculate_specific_b(DVS_test)
-f_test <- f(HDI_test, b_test)
-TRF_GS_test <- TRF_GS(reprduct_temp[day == DOY,]$Temp , 1000, GDD_START_HEADING, GDD_1_3)
-
-TRF_GS_test
-f_test
-DOY_planting <- 149
-weather_data[, Tt := 0]
-
-st<-reprduct_temp$DATE[1]
-ed <- reprduct_temp$DATE[nrow(reprduct_temp)]
-
-trf_gs <- as.double(rep(NA, ed-st+1))
-for (i in st:ed){
-
-    T <- hourly_temp[day == i,]$Temp  # Replace with actual temperature data
-    GDD <- reprduct_temp[DATE == i,]$GDD
-
-    hdi <- HDI(T, tau_cultivar)
-    DVS <- calculate_DVS(GDD, GDD_1_3, GDD_START_HEADING)
-    b <- calculate_specific_b(DVS)
-    f_hdi <- f(hdi, b)
-    trf_gs[i] <- f_hdi
-    
-    # daily_survival <- TRF_GS(T, GDD, GDD_START_HEADING, GDD_1_3)
-
-}
-1 - sum(1 - trf_gs[!is.na(trf_gs)])
-
-reprduct_temp
-
-
-# grain setting - function 11 - maybe unnecessary
-
-# Function to multiply all the penalties over the period of interest and subtract the value from GPP
-# calculate_penalty_and_adjust_GPP <- function(GPP = 1, start_day, end_day) {
-    total_penalty <- 1.0
-    for (i in start_day:end_day) {
-        T <- reprduct_temp[day == DOY,]$Temp  # Replace with actual temperature data
-        daily_penalty <- TRF_GS(T, GDD, GDD_START_HEADING, GDD_1_3)
-        total_penalty <- total_penalty * daily_penalty
-    }
-    adjusted_GPP <- GPP - total_penalty
-    # return(adjusted_GPP)
-# }
-calculate_penalty_and_adjust_GPP(1, 231, 240)
-
-
+ggsave("Temperature curve.svg", height = 7, width = 8)
 
